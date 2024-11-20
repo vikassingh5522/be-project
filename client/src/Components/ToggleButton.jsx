48a98862db46911ffa709037f2f7e87e62c42bf6
= <=>
2024-11-20 17:35:14 +0530

    minor UI changes

--- client/src/Components/ToggleButton.jsx
+++ client/src/Components/ToggleButton.jsx
@@ -5,7 +5,7 @@ const ToggleButton = ({ showWebcam, onClick }) => {
   return (
     <button
       onClick={onClick}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-4 rounded focus:outline-none focus:shadow-outline"
     >
       {showWebcam ? 'Hide Webcam' : 'Show Webcam'}
     </button>
